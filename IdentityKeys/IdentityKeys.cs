3fc6143651be2fb005030c9f3e9ca4205375e674
sambarnes <mistersamuelbarnes@gmail.com>
2019-03-12 16:37:39 -0500

    Initial commit

---
+++ IdentityKeys/IdentityKeys.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Security.Cryptography;
+using Base58Check;
+using Chaos.NaCl;
+
+namespace IdentityKeys
+{
+    public class PrivateIdentityKey
+    {
+        public static readonly byte[] PREFIX = new byte[] { 0x03, 0x45, 0xf3, 0xd0, 0xd6 };
+        public byte[] Seed { get; set; }
+
+        public PrivateIdentityKey()
+        {
+            Seed = new byte[32];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(Seed);
+        }
+
+        public PrivateIdentityKey(byte[] seed)
+        {
+            if (seed == null) {
+                throw new System.ArgumentException("Parameter cannot be null", "seed");
+            }
+            else if (seed.Length != 32)
+            {
+                throw new System.ArgumentException("Parameter must be 32 bytes long", "seed");
+            }
+            Seed = seed;
+        }
+
+        public PrivateIdentityKey(string keyString)
+        {
+            if (keyString == null) {
+                throw new System.ArgumentException("Parameter cannot be null", "keyString");
+            }
+            else if (keyString.Length != 55 || keyString.Substring(0, 5) != "idsec")
+            {
+                throw new System.ArgumentException("Invalid private key string", "keyString");
+            }
+            byte[] withPrefix;
+            try
+            {
+                withPrefix = Base58CheckEncoding.Decode(keyString);
+            }
+            catch (FormatException)
+            {
+                throw new System.ArgumentException("Invalid private key string", "keyString");
+            }
+            Seed = new byte[32];
+            System.Buffer.BlockCopy(withPrefix, PrivateIdentityKey.PREFIX.Length, Seed, 0, Seed.Length);
+        }
+        
+        public PublicIdentityKey GetPublicIdentityKey()
+        {
+            return new PublicIdentityKey(Ed25519.PublicKeyFromSeed(Seed));
+        }
+
+        public byte[] Sign(byte[] message)
+        {
+            return Ed25519.Sign(message, Ed25519.ExpandedPrivateKeyFromSeed(Seed));
+        }
+
+        override public string ToString()
+        {
+            byte[] withPrefix = new byte[PrivateIdentityKey.PREFIX.Length + Seed.Length];
+            System.Buffer.BlockCopy(PrivateIdentityKey.PREFIX, 0, withPrefix, 0, PrivateIdentityKey.PREFIX.Length);
+            System.Buffer.BlockCopy(Seed, 0, withPrefix, PrivateIdentityKey.PREFIX.Length, Seed.Length);
+            return Base58CheckEncoding.Encode(withPrefix);
+        }
+    }
+
+    public class PublicIdentityKey
+    {
+        public static readonly byte[] PREFIX = new byte[] { 0x03, 0x45, 0xef, 0x9d, 0xe0 };
+        public byte[] Bytes { get; set; }
+
+        public PublicIdentityKey(byte[] bytes)
+        {
+            if (bytes == null) {
+                throw new System.ArgumentException("Parameter cannot be null", "bytes");
+            }
+            else if (bytes.Length != 32)
+            {
+                throw new System.ArgumentException("Parameter must be 32 bytes long", "bytes");
+            }
+            Bytes = bytes;
+        }
+
+        public PublicIdentityKey(string keyString) {
+            if (keyString == null) {
+                throw new System.ArgumentException("Parameter cannot be null", "keyString");
+            }
+            else if (keyString.Length != 55 || keyString.Substring(0, 5) != "idpub")
+            {
+                throw new System.ArgumentException("Invalid public key string", "keyString");
+            }
+            byte[] withPrefix;
+            try
+            {
+                withPrefix = Base58CheckEncoding.Decode(keyString);
+            }
+            catch (FormatException)
+            {
+                throw new System.ArgumentException("Invalid public key string", "keyString");
+            }
+            Bytes = new byte[32];
+            System.Buffer.BlockCopy(withPrefix, PublicIdentityKey.PREFIX.Length, Bytes, 0, Bytes.Length);
+        }
+
+        public bool Verify(byte[] message, byte[] signature)
+        {
+            return Ed25519.Verify(signature, message, Bytes);
+        }
+
+        override public string ToString()
+        {
+            byte[] keyBody = new byte[PublicIdentityKey.PREFIX.Length + Bytes.Length];
+            System.Buffer.BlockCopy(PublicIdentityKey.PREFIX, 0, keyBody, 0, PublicIdentityKey.PREFIX.Length);
+            System.Buffer.BlockCopy(Bytes, 0, keyBody, PublicIdentityKey.PREFIX.Length, Bytes.Length);
+            return Base58CheckEncoding.Encode(keyBody);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,63 @@
+# .NET Identity Keys
+
+A small module of tools to generate and use key pairs for Factom Identities.
+
+## WARNING!
+
+Although this has been checked manually for functionality and compatibility with other existing implementations, no official unit tests have been added yet. Do not yet use in production.
+
+## Usage
+To install this module from nuget, run the following:
+```$ ```
+
+Generating a new random key pair and getting the idpub/idec key strings:
+```csharp
+PrivateIdentityKey priv = new PrivateIdentityKey();
+PublicIdentityKey pub = priv.GetPublicIdentityKey();
+Console.WriteLine("Private Key: " + priv.ToString());
+Console.WriteLine("Public Key: " + pub.ToString());
+```
+
+Signing a message and then verifying the signature:
+```csharp
+PrivateIdentityKey priv = new PrivateIdentityKey();
+PublicIdentityKey pub = priv.GetPublicIdentityKey();
+byte[] message = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 };
+byte[] signature = priv.Sign(message);
+
+if (pub.Verify(message, signature))
+    Console.WriteLine("Signature Valid!");
+else
+    Console.WriteLine("Signature Invalid!");
+```
+
+## Format of an Identity Key Pair
+*Note: the following text is taken from the [Application Identity Specification](https://github.com/FactomProject/FactomDocs/blob/FD-849_PublishNewIdentitySpec/ApplicationIdentity.md)*
+
+For Factom Application Identities, ed25519 keys are used to sign and verify messages. Rather than simply using raw 32 byte arrays for keys, the following encoding scheme is used: 
+
+Pseudo-code for constructing a private key string:
+```
+prefix_bytes = [0x03, 0x45, 0xf3, 0xd0, 0xd6]              // gives an "idsec" prefix once in base58 
+key_bytes = [32 bytes of raw private key]                  // the actual ed25519 private key seed
+checksum = sha256( sha256(prefix_bytes + key_bytes) )[:4]  // 4 byte integrity check on the previous 37 bytes
+
+idsec_key_string = base58( prefix_bytes + key_bytes + checksum )
+```
+
+Pseudo-code for constructing a public key string:
+```
+prefix_bytes = [0x03, 0x45, 0xef, 0x9d, 0xe0]              // gives an "idpub" prefix once in base58 
+key_bytes = [32 bytes of raw public key]                   // the actual ed25519 public key
+checksum = sha256( sha256(prefix_bytes + key_bytes) )[:4]  // 4 byte integrity check on the previous 37 bytes
+
+idpub_key_string = base58( prefix_bytes + key_bytes + checksum )
+```
+
+For the sake of human-readability, all characters must be in Bitcoin's base58 character set, the private key will always begin with "idsec", and the public key will always begin with "idpub". Additionally, the checksum at the end serves to signal that a user has incorrectly typed/copied their key.
+
+Example key pair for the private key of all zeros:
+- `idsec19zBQP2RjHg8Cb8xH2XHzhsB1a6ZkB23cbS21NSyH9pDbzhnN6 idpub2Cy86teq57qaxHyqLA8jHwe5JqqCvL1HGH4cKRcwSTbymTTh5n`
+
+Example key pair for the private key of all ones:
+- `idsec1ARpkDoUCT9vdZuU3y2QafjAJtCsQYbE2d3JDER8Nm56CWk9ix idpub2op91ghJbRLrukBArtxeLJotFgXhc6E21syu3Ef8V7rCcRY5cc`
